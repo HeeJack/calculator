4e8a44d99444267c70aa49f12c58f910dd6b3607
heejack <ljw6828@gmail.com>
2019-04-16 14:10:04 +0900

    add jacoco

--- src/test/java/com/ljw/calculator/CalculatorTest.java
+++ src/test/java/com/ljw/calculator/CalculatorTest.java
@@ -8,12 +8,15 @@ public class CalculatorTest{
 	public void testSum(){
 		assertEquals(5, calculator.sum(2,3));
 	}
+	@Test
 	public void testSub(){
-		assertEquals(2, calculator.sum(5,3));
+		assertEquals(2, calculator.sub(5,3));
 	}
+	@Test
 	public void testMul(){
 		assertEquals(6, calculator.mul(2,3));				       
 	}
+	@Test
 	public void testDiv(){
 		assertEquals(2, calculator.div(6,3));
 	}
