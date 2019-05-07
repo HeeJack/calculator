bac022424e89d9e202c4f2e438431039a42f6782
heejack <ljw6828@gmail.com>
2019-05-07 14:55:58 +0900

    calc3 check

--- src/main/java/com/ljw/calculator/Calculator.java
+++ src/main/java/com/ljw/calculator/Calculator.java
@@ -1,4 +1,5 @@
 package com.ljw.calculator;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 /**
  * This is Calculator Service Class
@@ -6,15 +7,19 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class Calculator{
+	@Cacheable("sum")
 	int sum(int a, int b){
 		return a+b;
 	}
+	@Cacheable("sub")
 	int sub(int a, int b){
 		return a-b;
 	}
+	@Cacheable("mul")
 	int mul(int a, int b){
 		return a*b;
 	}
+	@Cacheable("div")
 	int div(int a, int b){
 		if(a != 0){
 			return a/b;
