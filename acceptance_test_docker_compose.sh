9a9fbf352e96b79024071c9406366cd8ea8fd944
heejack <ljw6828@gmail.com>
2019-05-09 14:55:05 +0900

    acceptance

--- acceptance_test_docker_compose.sh
+++ acceptance_test_docker_compose.sh
@@ -3,3 +3,4 @@
 #test $(curl --silent $(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}' bridge):8090/sum?a=1\&b=2) -eq 3  # true on docker
 CALCULATOR_PORT=$(docker-compose port calculator 8090 | cut -d: -f2)
 test $(curl --silent localhost:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
+echo $(curl --silent localhost:$CALCULATOR_PORT/sum?a=1\&b=2)
