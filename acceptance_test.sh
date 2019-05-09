81edf5e72ebf7c2a4589b14c48a00edb257fb50c
heejack <ljw6828@gmail.com>
2019-05-09 14:22:07 +0900

    acceptance check

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,3 @@
-$(curl --silent localhost:8090/sum?a=1\&b=2) -eq 3 || \    # true when it runs on host
-	test $(curl --silent $(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}' bridge):8090/sum?a=1\&b=2) -eq 3  # true on docker
+test --slient $(curl --silent localhost:8090/sum?a=1\&b=2) -eq 3 || \    # true when it runs on host
+test --slient $(curl --silent $(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}' bridge):8090/sum?a=1\&b=2) -eq 3  # true on docker
 
